f4d160edc176da33219e90286b9b600afd2efeac
sadafamiri <sadafamiri090@gmail.com>
2026-03-15 22:07:14 +0430

    tracker commit

--- goal-tracker-project/src/App.jsx
+++ goal-tracker-project/src/App.jsx
@@ -1,35 +1,40 @@
-import Layout from './pages/Layout'
-import Dashboard from './pages/Dashboard'
-import Categories from "./pages/Categories"
-import GoalDetails from './pages/GoalDetails'
-import Settings from './pages/Settings'
-import NotFound from './pages/NotFound'
-import Goals from './pages/Goals'
-import NewGoal from './pages/NewGoal'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import { GoalsProvider } from './context/GoalsContext'
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import Categories from "./pages/Categories";
+import GoalDetails from "./pages/GoalDetails";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+import Goals from "./pages/Goals";
+import NewGoal from "./pages/NewGoal";
+
+import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
+
+import { GoalsProvider } from "./context/GoalsContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { LanguageProvider } from "./context/LanguageContext";
 
 function App() {
-
-
   return (
-    <GoalsProvider>
-      <div>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/goals" element={<Goals />} />
-            <Route path="/goals/new" element={<NewGoal />} />
-            <Route path="/goals/:id" element={<GoalDetails />} />
-            <Route path="/categories" element={<Categories />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </div>
-    </GoalsProvider>
-  )
+    <ThemeProvider>
+      <LanguageProvider>
+        <GoalsProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/goals" element={<Goals />} />
+              <Route path="/goals/new" element={<NewGoal />} />
+              <Route path="/goals/:id" element={<GoalDetails />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </GoalsProvider>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
